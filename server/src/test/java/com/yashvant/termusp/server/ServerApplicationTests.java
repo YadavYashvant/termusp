6094d13af830b098231578c6c4555ea9e23352cc
YadavYashvant <yashvantyadav855@gmail.com>
2024-10-31 22:28:44 +0530

    making backend

---
+++ server/src/test/java/com/yashvant/termusp/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.termusp.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
